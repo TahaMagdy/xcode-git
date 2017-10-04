10e2a1c0864d7d1f14e109053b0da15cd1020fd1
TahaMagdy <tahamagdy314@gmail.com>
2017-10-04 19:33:20 +0200

    Initial Commit

---
+++ xcode git pro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xcode git pro
+//
+//  Created by Taha Magdy on 10/4/17.
+//  Copyright © 2017 Taha Magdy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
