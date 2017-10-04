c2e6bc2adaf936abb7d3c781bc3e09427c58b6d8
TahaMagdy <tahamagdy314@gmail.com>
2017-10-04 19:41:49 +0200

    إضافة بدوي

--- xcode git pro/ViewController.swift
+++ xcode git pro/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    func بدوي() -> String {
+        return "الصوت في إيه؟!"
+    }
 
 
 
